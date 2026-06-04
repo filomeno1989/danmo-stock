679ed97bd8f8194c1302f8c0f086d782c2e0b73f
filomeno1989 <filomeno1989@gmail.com>
2026-06-04 10:19:55 +0200

    Update supabase.js

--- supabase.js
+++ supabase.js
@@ -1,5 +1,5 @@
 const SUPABASE_URL = 'https://czgnbzxoeylicrqjvncd.supabase.co';
-const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6Z25ienhvZXlsaWNycWp2bmNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDUzMzE3MDYsImV4cCI6MjA2MDkwNzcwNn0.B7tHRFLwtyVGGRObVmGTQZPZFTMaZpFLNMfxXIBpo7Y';
+const SUPABASE_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN6Z25ienhvZXlsaWNycWp2bmNkIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzkxNzcxNzcsImV4cCI6MjA5NDc1MzE3N30.g_msMjIqje6UtMf4Cy-eTodGlRKVWIa5Q0-9s5YpJJw';
 
 const db = {
   async get(tabela, filtros = {}) {
